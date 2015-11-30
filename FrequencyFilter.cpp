f7921fa0b384caa10d5756a0bff7c54919f3afc0
Marysiaa <marysia.pieniazek@o2.pl>
2015-11-30 13:01:46 +0200

    poprawki

--- FrequencyFilter.cpp
+++ FrequencyFilter.cpp
@@ -10,3 +10,14 @@ short* FrequencyFilter::Filter(short * samples, int numberOfSamples, int min, in
 	}
 	return samples;
 }
+
+Complex* FrequencyFilter::Filter(Complex* harmonic, int numberOfSamples, int min, int max) {
+
+	for (int i = 0; i < numberOfSamples; i++) {
+		if (harmonic[i].im > max)
+			harmonic[i].im = 0;
+		else if (harmonic[i].im > max)
+			harmonic[i].im = 0;
+	}
+	return harmonic;
+}
--- FrequencyFilter.h
+++ FrequencyFilter.h
@@ -1,9 +1,11 @@
 #ifndef FREQ_H
 #define FREQ_H
+#include"Complex.h"
 
 class FrequencyFilter {
 public:
 	short* Filter(short * samples, int numberOfSamples, int min, int max);
+	Complex* Filter(Complex* harmonic, int numberOfSamples, int min, int max);
 };
 
 #endif
